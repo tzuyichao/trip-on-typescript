31683f633f10fd5edc4fe8e10272d2320015ce23
TzuYi Chao <tzuyichao0313@gmail.com>
2023-07-14 12:51:52 +0800

    move to folder

--- enum-1.js
+++
@@ -1,7 +0,0 @@
-var Vehicle;
-(function (Vehicle) {
-    Vehicle["Car"] = "Car";
-    Vehicle["Bicycle"] = "Bike";
-    Vehicle["Truck"] = "Pickup Truck";
-    Vehicle["Bus"] = "Coach";
-})(Vehicle || (Vehicle = {}));
