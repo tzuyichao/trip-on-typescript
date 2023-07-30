e283fef2a00921f2e651139f10daaa21ea12df55
TzuYi Chao <tzuyichao0313@gmail.com>
2023-07-30 20:33:14 +0800

    support dark theme

--- reactjs/realtime-weather-app/src/App.js
+++ reactjs/realtime-weather-app/src/App.js
@@ -4,9 +4,30 @@ import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg'
 import { ReactComponent as RainIcon } from './images/rain.svg'
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg'
 import { ReactComponent as RefreshIcon } from './images/refresh.svg'
+import { ThemeProvider } from '@emotion/react'
+
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
 
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -16,21 +37,21 @@ const Container = styled.div`
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
   box-sizing: border-box;
   padding: 30px 15px;
 `;
 
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `;
 
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `;
 
@@ -42,7 +63,7 @@ const CurrentWeather = styled.div`
 `;
 
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -58,8 +79,9 @@ const AirFlow = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
+
   svg {
     width: 25px;
     height: auto;
@@ -72,12 +94,17 @@ const Rain = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
+
   svg {
     width: 25px;
     height: auto;
     margin-right: 30px;
   }
+`;
+
+const DayCloudy = styled(DayCloudyIcon)`
+  flex-basis: 30%;
 `;
 
 const Refresh = styled.div`
@@ -87,7 +114,8 @@ const Refresh = styled.div`
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
+
   svg {
     margin-left: 10px;
     width: 15px;
@@ -98,27 +126,29 @@ const Refresh = styled.div`
 
 function App() {
   return (
-    <Container>
-      <WeatherCard>
-        <Location>高雄市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            33<Celsius>°C</Celsius>
-          </Temperature>
-          <DayCloudyIcon />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon /> 23 m/h 
-        </AirFlow>
-        <Rain> 
-          <RainIcon />48% 
-        </Rain>
-        <Refresh> 
-          最後觀測時間:上午 12:03 <RefreshIcon />
-        </Refresh>
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme.dark}>
+      <Container>
+        <WeatherCard>
+          <Location theme="dark">高雄市</Location>
+          <Description>多雲時晴</Description>
+          <CurrentWeather>
+            <Temperature>
+              33<Celsius>°C</Celsius>
+            </Temperature>
+            <DayCloudyIcon />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon /> 23 m/h 
+          </AirFlow>
+          <Rain> 
+            <RainIcon />48% 
+          </Rain>
+          <Refresh> 
+            最後觀測時間:上午 12:03 <RefreshIcon />
+          </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   );
 }
 
